7f0709294b4985a0910b1857c9dcf9f75185177c
Yun Seob Lee <fhzotxldj@gmail.com>
2024-05-01 19:49:07 +0900

    refactor to remove warning

--- util.py
+++ util.py
@@ -12,7 +12,7 @@ def encode_jwt(user_id, role):
     payload = {
         'user_id': user_id,
         'role': role,
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
+        'exp': datetime.datetime.now(datetime.UTC) + datetime.timedelta(seconds=60 * 60 * 24)
     }
     jwt_token = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
     return jwt_token
