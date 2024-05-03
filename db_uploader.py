8ef3ccc5499d373626bf442a0797832128375c02
Yun Seob Lee <fhzotxldj@gmail.com>
2024-05-03 11:47:55 +0900

    refactoring db_uploader to bulk insert

--- db_uploader.py
+++ db_uploader.py
@@ -2,7 +2,6 @@
 테스트용 유저 데이터를 만드는 데 사용됩니다. `users.csv`에서 데이터를 가져옵니다.
 """
 
-from sqlalchemy.engine.interfaces import DBAPICursor
 from sqlalchemy.dialects.postgresql import insert
 from database import engine, Base
 import csv
@@ -13,28 +12,32 @@ import datetime
 load_dotenv()
 
 
-def _insert_user(data, cursor: DBAPICursor):
-    query = 'INSERT INTO users(id, user_id, password, role) VALUES (%s, %s, %s, %s) ON CONFLICT DO NOTHING;'
-
-    cursor.execute(query, data)
-
-
 def init_data():
     if os.environ.get('environment', 'dev') == 'test':
         return
 
     with open('data/users.csv', 'r', encoding='utf-8') as data:
         print('---inserting user data started---')
-        conn = engine.raw_connection()
+        conn = engine.connect()
 
-        cursor = conn.cursor()
         data = csv.reader(data)
 
+        users = []
         for line in data:
-            _insert_user(line, cursor)
+            users.append({
+                'id': line[0],
+                'user_id': line[1],
+                'password': line[2],
+                'role': line[3]
+            })
 
-        print('---inserting user data ended---')
+        user_table = Base.metadata.tables['users']
+
+        stmt = insert(user_table).values(users).on_conflict_do_nothing()
+        conn.execute(stmt)
+
         conn.commit()
+        conn.close()
 
     with engine.connect() as conn:
         exam_table = Base.metadata.tables['exam_schedules']
@@ -49,3 +52,5 @@ def init_data():
         conn.execute(exam1_insert_stmt)
         conn.execute(exam2_insert_stmt)
         conn.commit()
+
+    print('---inserting user data ended---')
